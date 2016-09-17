c13e242a9b01a245123ca0a7ad444f5acdcdc436
Michael Dove <michael@michaeldove.com.au>
2016-09-18 00:02:59 +1000

    Moved VWTP message handling into VWTP2 class.

--- vagagent.py
+++ vagagent.py
@@ -59,9 +59,33 @@ class VWTP2(object):
 
     def __init__(self, bus):
         self.bus = bus
+        self.seq = 0
+
+    def can_handle_message(self, can_message):
+        return can_message.arbitration_id == MY_VWTP_CAN_ID
+
+    def handle_message(self, can_message):
+        """Handle VWTP frame"""
+        # we're a VWTP session
+        vwtp_opcode = data[0] >> 4
+        vwtp_seq = data[0] & 0xf
+        if vwtp_opcode == VWTP_OPCODE_LAST_PACKET_ACK:
+            # Act on packet and respond with ack
+            vwtp_next_seq = (vwtp_seq + 1) & 0xf
+            self.send_ack(vwtp_next_seq)
+        if vwtp_seq == 0:
+            payload_len = data[2] # refactor this to a intermessage context
+            # What if payload length is greater than message size?
+            # Should add message payload to a read buffer and send to a app
+            # protocol handler once transfer is complete.
+            kwp_payload = data[VWTP_PAYLOAD_INDEX:VWTP_PAYLOAD_INDEX+payload_len]
+            kwp_opcode = kwp_payload[0]
+            kwp_param = kwp_payload[1]
+            if kwp_opcode == KWP_READ_ECU_IDENTIFICATION_OPCODE:
+                if kwp_param == KWP_ITEM_NUMBER_PARAM:
+                    send_item_number(tp)
 
     def send(self, message):
-        seq = 0
         payload_start_index = 0
         payload = message.bytes()
         message_length = len(payload)
@@ -69,7 +93,7 @@ class VWTP2(object):
 
         while remaining_length:
             vwtp_header_length = VWTP_FRAME_HEADER_LENGTH
-            first_frame = seq == 0
+            first_frame = self.seq == 0
             if first_frame:
                 vwtp_header_length = VWTP_FRAME_HEADER_FIRST_LENGTH
             vwtp_payload_length = VWTP_FRAME_LENGTH - vwtp_header_length
@@ -78,7 +102,7 @@ class VWTP2(object):
             opcode = VWTP_OPCODE_MORE_PACKETS_NOACK
             if last_frame:
                 opcode = VWTP_OPCODE_LAST_PACKET_ACK
-            first_byte = opcode << 4 | seq
+            first_byte = opcode << 4 | self.seq
 
             data = bytearray([first_byte])
             if first_frame:
@@ -94,7 +118,7 @@ class VWTP2(object):
             self.bus.send(message)
             remaining_length -= len(frame_payload_bytes)
             payload_start_index += len(frame_payload_bytes)
-            seq = (seq + 1) & 0xf
+            self.seq = (self.seq + 1) & 0xf
 
     def send_ack(self, seq):
         message = Message(extended_id=False,
@@ -121,46 +145,32 @@ def send_item_number(tp):
 bus = can.interface.Bus(can_interface, bustype='socketcan')
 tp = VWTP2(bus)
 for message in bus:
-  msg_can_id = message.arbitration_id
-  data = message.data
-  if msg_can_id == TESTER_CAN_ID:
-    # we're in raw can frame mode with a tester connected
-    if data[0] == ECU_LOGICAL_ADDRESS:
-      # OK, the tester is talking to us (ECU)
-      if data[1] == SETUP_REQUEST_OPCODE:
-        dest_vwtp_can_id_invalid = data[5] & CAN_ID_INVALID_MASK
-        if dest_vwtp_can_id_invalid:
-          print "Error: Invalid destination VWTP CAN ID."
-          continue
-        dest_vwtp_can_id = struct.unpack('<H', data[4:6])[0]
-        if data[6] != KWP2000_APP_TYPE:
-          print "Error: Expecting KWP2000 app type."
-          continue
-        response_data = [0x00, 
-                         SETUP_RESPONSE_OPCODE, 
-                         dest_vwtp_can_id & 0xff, 
-                         dest_vwtp_can_id >> 8, 
-                         MY_VWTP_CAN_ID & 0xff, 
-                         MY_VWTP_CAN_ID >> 8,
-                         KWP2000_APP_TYPE]
-        message = Message(extended_id=False,
-                          arbitration_id=TESTER_CAN_ID+1,
-                          data=response_data) 
-        bus.send(message)
-        continue
-  if msg_can_id == MY_VWTP_CAN_ID:
-    # we're a VWTP session
-    vwtp_opcode = data[0] >> 4
-    vwtp_seq = data[0] & 0xf
-    if vwtp_opcode == VWTP_OPCODE_LAST_PACKET_ACK:
-        # Act on packet and respond with ack
-        vwtp_next_seq = (vwtp_seq + 1) & 0xf
-        tp.send_ack(vwtp_next_seq)
-        if vwtp_seq == 0:
-            payload_len = data[2] # refactor this to a intermessage context
-            kwp_payload = data[VWTP_PAYLOAD_INDEX:VWTP_PAYLOAD_INDEX+payload_len]
-            kwp_opcode = kwp_payload[0]
-            kwp_param = kwp_payload[1]
-            if kwp_opcode == KWP_READ_ECU_IDENTIFICATION_OPCODE:
-                if kwp_param == KWP_ITEM_NUMBER_PARAM:
-                    send_item_number(tp)
+    msg_can_id = message.arbitration_id
+    data = message.data
+    if msg_can_id == TESTER_CAN_ID:
+        # we're in raw can frame mode with a tester connected
+        if data[0] == ECU_LOGICAL_ADDRESS:
+            # OK, the tester is talking to us (ECU)
+            if data[1] == SETUP_REQUEST_OPCODE:
+                dest_vwtp_can_id_invalid = data[5] & CAN_ID_INVALID_MASK
+                if dest_vwtp_can_id_invalid:
+                    print "Error: Invalid destination VWTP CAN ID."
+                    continue
+            dest_vwtp_can_id = struct.unpack('<H', data[4:6])[0]
+            if data[6] != KWP2000_APP_TYPE:
+                print "Error: Expecting KWP2000 app type."
+                continue
+            response_data = [0x00, 
+                             SETUP_RESPONSE_OPCODE, 
+                             dest_vwtp_can_id & 0xff, 
+                             dest_vwtp_can_id >> 8, 
+                             MY_VWTP_CAN_ID & 0xff, 
+                             MY_VWTP_CAN_ID >> 8,
+                             KWP2000_APP_TYPE]
+            message = Message(extended_id=False,
+                              arbitration_id=TESTER_CAN_ID+1,
+                              data=response_data) 
+            bus.send(message)
+            continue
+    if tp.can_handle_message(message):
+        tp.handle_message(message)
